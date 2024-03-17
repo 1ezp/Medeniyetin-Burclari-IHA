3e22558d2f40e3b67743e6556929b14745836e3d
DIAMOND754 <xxmohammadxxxx754@gmail.com>
2024-03-17 13:58:00 +0300

    Modified AICameraSimulator.ino - Added I2C communication

--- PID/AICameraSimulator/AICameraSimulator.ino
+++ PID/AICameraSimulator/AICameraSimulator.ino
@@ -1,24 +1,29 @@
 // This sketch has been made by github.com/DIAMOND754
 // This sketch is going to emulate an AI camera and sends the location of a target using (x, y) coordinates
 
-// Currently, thhis sketch is going to work with 1 potentiometer and is going to send this data over I2C
+// Currently, this sketch is going to work with 1 potentiometer and is going to send this data over I2C
 
 // -------------------------------------------------------------------------
 // -------------------------------------------------------------------------
 
-const int potentiometerPin = A5;
+#include <SoftwareSerial.h>
+SoftwareSerial espSerial(A4, A5);
+
+const int potentiometerPin = A0;
 int bilgi;
 
 void setup(){
 
   pinMode(potentiometerPin, INPUT);
   Serial.begin(9600);
+  espSerial.begin(9600);
 }
 
 void loop(){
 
   bilgi = analogRead(potentiometerPin);
   bilgi = map(bilgi, 0, 1023, 0, 360);
+  espSerial.println(bilgi);
   Serial.println(bilgi);
   delay(100);
 }
