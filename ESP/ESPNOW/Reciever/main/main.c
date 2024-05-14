323cfffb42ad64786c5ff1997f6d0d74d231712d
DIAMOND754 <xxmohammadxxxx754@gmail.com>
2024-05-14 23:15:10 +0300

    Modified Receiver - Added Manual Servo Control With Auto Balancer && Fixed Syntax For Pixhawlk And PID Control

--- ESP/ESPNOW/Reciever/main/main.c
+++ ESP/ESPNOW/Reciever/main/main.c
@@ -131,6 +131,10 @@ static esp_err_t esp_now_send_data(const uint8_t *peer_addr, const uint8_t *data
 
 int data[2] = {0, 0};
 
+bool isManual = true;
+bool isPixhawlk = false;
+bool isPID = false;
+
 void recv_cb(const esp_now_recv_info_t * esp_now_info, const uint8_t *bilgi, int data_len){
 
     // ESP_LOGI(TAG, "Data Received: " MACSTR " %s", MAC2STR(esp_now_info->src_addr), bilgi);
@@ -142,14 +146,15 @@ void recv_cb(const esp_now_recv_info_t * esp_now_info, const uint8_t *bilgi, int
 // --------------------------------------------------
 
 
-// ---------------------rlServo----------------------
+// ---------------------Servo------------------------
 
 #define SERVO_MIN_PULSEWIDTH_US     500  // Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH_US     2500  // Maximum pulse width in microsecond
 #define SERVO_MIN_DEGREE            -90   // Minimum angle
 #define SERVO_MAX_DEGREE            90    // Maximum angle
 
-#define SERVO_PULSE_GPIO             15        // GPIO connects to the PWM signal line
+#define SERVO_PULSE_GPIO_RL             15        // GPIO connects to the PWM signal line
+#define SERVO_PULSE_GPIO_UD             16        // GPIO connects to the PWM signal line
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
@@ -161,17 +166,31 @@ static inline uint32_t example_angle_to_compare(int angle){
 // --------------------------------------------------
 
 
-// ---------------------Main-------------------------
+// ------------------ServoTask-----------------------
 
-void app_main(void){
+bool isManualTaskOpen = true;
 
-    ESP_ERROR_CHECK(init_wifi());
-    ESP_ERROR_CHECK(init_esp_now());
-    ESP_ERROR_CHECK(register_peer(peer_mac));
+short int currentXAngle = 0;
+short int currentYAngle = 0;
+
+long long int previousRlMillis = 0;
+long long int previousUdMillis = 0;
+
+long long int sensetivityX = 0;
+long long int sensetivityY = 0;
+
+#define sensitivityTimeout 75
+#define servoPillTimeout 100
+
+void servoWrite(void *pvParameters){
+
+    // -------Task---------
+
+    isManualTaskOpen = false;
+
+    // --------------------
 
     // ------rlServo-------
-
-    // Create timer and operator
 
     mcpwm_timer_handle_t rlTimer = NULL;
     mcpwm_timer_config_t rlTimer_config = {
@@ -189,10 +208,7 @@ void app_main(void){
     };
     ESP_ERROR_CHECK(mcpwm_new_operator(&rlOper_config, &rlOper));
 
-    // Connect timer and operator
     ESP_ERROR_CHECK(mcpwm_operator_connect_timer(rlOper, rlTimer));
-
-    // Create comparator and generator from the operator
 
     mcpwm_cmpr_handle_t rlComparator = NULL;
     mcpwm_comparator_config_t rlComparator_config = {
@@ -202,68 +218,253 @@ void app_main(void){
 
     mcpwm_gen_handle_t rlGenerator = NULL;
     mcpwm_generator_config_t rlGenerator_config = {
-        .gen_gpio_num = SERVO_PULSE_GPIO,
+        .gen_gpio_num = SERVO_PULSE_GPIO_RL,
     };
     ESP_ERROR_CHECK(mcpwm_new_generator(rlOper, &rlGenerator_config, &rlGenerator));
 
-    // Set generator action on timer and compare event
-
-    // Go high on counter empty
     ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(rlGenerator,
                                                               MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
-    // Go low on compare threshold
     ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(rlGenerator,
                                                                 MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, rlComparator, MCPWM_GEN_ACTION_LOW)));
 
-    // Enable and start timer
     ESP_ERROR_CHECK(mcpwm_timer_enable(rlTimer));
     ESP_ERROR_CHECK(mcpwm_timer_start_stop(rlTimer, MCPWM_TIMER_START_NO_STOP));
 
     // --------------------
 
-    int currentXAngle = 0;
-    int currentYAngle = 0;
+    // ------udServo-------
 
-    while(true){
+    mcpwm_timer_handle_t udTimer = NULL;
+    mcpwm_timer_config_t udTimer_config = {
+        .group_id = 1,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
+        .period_ticks = SERVO_TIMEBASE_PERIOD,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&udTimer_config, &udTimer));
 
-        if(data[0] == 1){
+    mcpwm_oper_handle_t udOper = NULL;
+    mcpwm_operator_config_t udOper_config = {
+        .group_id = 1, // operator must be in the same group to the timer
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&udOper_config, &udOper));
 
-            currentXAngle+=5;
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(udOper, udTimer));
+
+    mcpwm_cmpr_handle_t udComparator = NULL;
+    mcpwm_comparator_config_t udComparator_config = {
+        .flags.update_cmp_on_tez = true,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_comparator(udOper, &udComparator_config, &udComparator));
+
+    mcpwm_gen_handle_t udGenerator = NULL;
+    mcpwm_generator_config_t udGenerator_config = {
+        .gen_gpio_num = SERVO_PULSE_GPIO_UD,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(udOper, &udGenerator_config, &udGenerator));
+
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(udGenerator,
+                                                              MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(udGenerator,
+                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, udComparator, MCPWM_GEN_ACTION_LOW)));
+
+    ESP_ERROR_CHECK(mcpwm_timer_enable(udTimer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(udTimer, MCPWM_TIMER_START_NO_STOP));
+
+    // --------------------
+
+    // -------Manual-------
+
+    sensetivityX = (esp_timer_get_time() / 1000);
+    sensetivityY = (esp_timer_get_time() / 1000);
+
+    while(isManual){
+
+    // ---------X----------
+
+        // When the joystick is in the middle
+        if(data[0] == 0){
+
+            if((esp_timer_get_time() / 1000) - previousRlMillis >= servoPillTimeout){
+
+                if(currentXAngle < 0){
+
+                    currentXAngle+=5;
+                }
+                else if(currentXAngle > 0){
+
+                    currentXAngle-=5;
+                }
+
+                previousRlMillis = (esp_timer_get_time() / 1000);
+            }
         }
+
+        // When the joystick is in the right
+        else if(data[0] == 1){
+
+            previousRlMillis = esp_timer_get_time() / 1000;
+
+            if((esp_timer_get_time() / 1000) - sensetivityX >= sensitivityTimeout){
+
+                currentXAngle+=5;
+                sensetivityX = (esp_timer_get_time() / 1000);
+            }
+        }
+
+        // When the joystick is in the left
         else if(data[0] == 2){
 
-            currentXAngle-=5;
+            previousRlMillis = esp_timer_get_time() / 1000;
+
+            if((esp_timer_get_time() / 1000) - sensetivityX >= sensitivityTimeout){
+
+                currentXAngle-=5;
+                sensetivityX = (esp_timer_get_time() / 1000);
+            }
         }
 
-        if(data[1] == 1){
+        // --------------------
 
-            currentYAngle+=5;
+        // ---------Y----------
+
+        // When the joystick is in the middle
+        if(data[1] == 0){
+
+            if((esp_timer_get_time() / 1000) - previousUdMillis >= servoPillTimeout){
+
+                if(currentYAngle < 0){
+
+                    currentYAngle+=5;
+                }
+                else if(currentYAngle > 0){
+
+                    currentYAngle-=5;
+                }
+
+                previousUdMillis = (esp_timer_get_time() / 1000);
+            }
         }
+
+        // When the joystick is in the bottom
+        else if(data[1] == 1){
+
+            previousUdMillis = esp_timer_get_time() / 1000;
+
+            if((esp_timer_get_time() / 1000) - sensetivityY >= sensitivityTimeout){
+
+                currentYAngle-=5;
+                sensetivityY = (esp_timer_get_time() / 1000);
+            }
+        }
+
+        // When the joystick is in the top
         else if(data[1] == 2){
 
-            currentYAngle-=5;
+            previousUdMillis = esp_timer_get_time() / 1000;
+
+            if((esp_timer_get_time() / 1000) - sensetivityY >= sensitivityTimeout){
+
+                currentYAngle+=5;
+                sensetivityY = (esp_timer_get_time() / 1000);
+            }
         }
 
-        if(currentXAngle < -70){
+        // --------------------
 
-            currentXAngle = -70;
-        }
-        else if(currentXAngle > 70){
+        // -------Range---------
+
+        if(currentXAngle > 70){
 
             currentXAngle = 70;
         }
+        else if(currentXAngle < -70){
 
-        if(currentYAngle < -70){
-
-            currentYAngle = -70;
+            currentXAngle = -70;
         }
-        else if(currentYAngle > 70){
+        if(currentYAngle > 70){
 
             currentYAngle = 70;
         }
+        else if(currentYAngle < -70){
 
-        printf("X: %d, Y: %d\n", currentXAngle, currentYAngle);
+            currentYAngle = -70;
+        }
+
+        // --------------------
+
+        // -------Write--------
+
         ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(rlComparator, example_angle_to_compare(currentXAngle)));
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(udComparator, example_angle_to_compare(currentYAngle)));
+        delay(10);
+
+        // --------------------
+
+    }
+
+    // --------------------
+
+    // --------Task---------
+
+    isManualTaskOpen = true;
+    vTaskDelete(NULL);
+
+    // --------------------
+}
+
+// --------------------------------------------------
+
+
+// ---------------------Main-------------------------
+
+#define pixhawlkPin 4
+
+void app_main(void){
+
+    ESP_ERROR_CHECK(init_wifi());
+    ESP_ERROR_CHECK(init_esp_now());
+    ESP_ERROR_CHECK(register_peer(peer_mac));
+
+    // --------GPIO--------
+
+    gpio_set_direction(pixhawlkPin, GPIO_MODE_OUTPUT);
+
+    // --------------------
+
+    while(true){
+
+        // -------Manual-------
+
+        if(isManual){
+
+            gpio_set_level(pixhawlkPin, 0);
+
+            if(isManualTaskOpen){
+
+                previousRlMillis = esp_timer_get_time() / 1000;
+                xTaskCreate(servoWrite, "servoWrite", 4096, NULL, 1, NULL);
+            }
+        }
+
+        // -------Auto---------
+
+        else if(isPixhawlk){
+
+            isManualTaskOpen = true;
+        }
+
+        // --------------------
+
+        // --------PID---------
+
+        else if(isPID){
+
+            isManualTaskOpen = true;
+        }
+
+        // --------------------
 
         delay(10);
     }
