fc91483b95dd5d56b333046cc294543d3fc83a6e
HikmeT <hekmt111@gmail.com>
2024-06-18 19:04:54 +0300

    added target x-y getting it from socketServer

--- MedeniyetinApp/Core/API.cs
+++ MedeniyetinApp/Core/API.cs
@@ -11,8 +11,6 @@ namespace MedeniyetinApp.Core
 {
 
     
-
-
     public class API
     {
 
--- MedeniyetinApp/Core/Server.cs
+++ MedeniyetinApp/Core/Server.cs
@@ -16,13 +16,21 @@ namespace MedeniyetinApp.Core
         public double Longitude { get; set; }
     }
 
+    public class TargetInfo
+    {
+        [JsonProperty("x")]
+        public double x { get; set; }
+
+        [JsonProperty("y")]
+        public double y { get; set; }
+    }
+
     public class Server 
     {
         private readonly TcpClient client;
         private readonly NetworkStream stream;
         private static readonly string ip = "127.0.0.1";
         private static readonly int port = 12345;
-        private bool disposed = false;
 
         public Server()
         {
@@ -38,7 +46,6 @@ namespace MedeniyetinApp.Core
                 byte[] requestData = Encoding.UTF8.GetBytes(request);
                 stream.Write(requestData, 0, requestData.Length);
 
-                // Read the response
                 byte[] buffer = new byte[1024];
                 int bytesRead = stream.Read(buffer, 0, buffer.Length);
                 string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
@@ -64,7 +71,6 @@ namespace MedeniyetinApp.Core
                 byte[] requestData = Encoding.UTF8.GetBytes(request);
                 stream.Write(requestData, 0, requestData.Length);
 
-                // Read the response
                 byte[] buffer = new byte[1024];
                 int bytesRead = stream.Read(buffer, 0, buffer.Length);
                 string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
@@ -90,12 +96,36 @@ namespace MedeniyetinApp.Core
                 byte[] requestData = Encoding.UTF8.GetBytes(request);
                 stream.Write(requestData, 0, requestData.Length);
 
-                // Read the response
                 byte[] buffer = new byte[1024];
                 int bytesRead = stream.Read(buffer, 0, buffer.Length);
                 string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                 GPSInfo gpsInfo = JsonConvert.DeserializeObject<GPSInfo>(message);
                 return gpsInfo;
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"SocketException: {se.Message}");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+                return null;
+            }
+        }
+        public TargetInfo TargetData()
+        {
+            try
+            {
+                string request = "Target";
+                byte[] requestData = Encoding.UTF8.GetBytes(request);
+                stream.Write(requestData, 0, requestData.Length);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                TargetInfo Info = JsonConvert.DeserializeObject<TargetInfo>(message);
+                return Info;
             }
             catch (SocketException se)
             {
