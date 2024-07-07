961bed26a04fc5f27b8f4d863323f139b0c5fd25
HikmeT <hekmt111@gmail.com>
2024-07-07 15:02:14 +0300

    replace the flask server with new code to run all receivers and senders

--- Communication-System/main.py
+++ Communication-System/main.py
@@ -1,39 +1,14 @@
-
 from data import *
-from fastapi import FastAPI,Request
+
+import serial
 from threading import Thread
 import usb 
-import uvicorn
-from pydantic import BaseModel
-#Thread(target=usb.start).start()
-print("Working")
 
-app = FastAPI()
-
-class IHAData(BaseModel):
-    Lat : float = -1
-    Long : float = -1
+from ..trackSystem import main as trackSystem
 
 
-@app.get("/")
-def Home():
-    return "fuck off pls"
+Thread(target=usb.main).start()
+Thread(target=serial.main).start()
 
-
-@app.get("/IHA")
-def IHALat():
-    return IHA
-
-@app.post("/IHA/5")
-async def IHALatss(request: IHAData):
-    #print(await request.body())
-    IHA.update({"Lat": request.Lat, "Long": request.Long})      
-    return request
-
-@app.get("/IKA")
-def IHALat():
-    return IKA
-
-if __name__ == '__main__':
-    uvicorn.run(app,port=80)
-      
+while True:
+    print("mode:",MODE["value"])
