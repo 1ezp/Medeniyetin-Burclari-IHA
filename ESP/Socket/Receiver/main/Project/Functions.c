b0c98649f4101215c34323fffd3f46a0ed7fa04a
DIAMOND <XXMOHAMMADXxXx754@gmail.com>
2024-07-07 21:45:24 +0300

    Added Functions.c

---
+++ ESP/Socket/Receiver/main/Project/Functions.c
@@ -0,0 +1,25 @@
+// ---------------------GUARD------------------------
+#ifndef Functions
+#define Functions
+// --------------------------------------------------
+
+
+// Data
+int data[6] = {1, 1, 1, 1, 1, 1};
+
+void analogWrite(int mode, int channel, int value){
+
+    ESP_ERROR_CHECK(ledc_set_duty(mode, channel, value));
+    ESP_ERROR_CHECK(ledc_update_duty(mode, channel));
+}
+
+void delay(int ms){ vTaskDelay(ms / portTICK_PERIOD_MS); }
+
+void digitalWrite(int pin, int state){ gpio_set_level(pin, state); }
+
+void extractNumbers(const char* input, int* num1, int* num2, int* num3, int* num4, int* num5, int* num6){if(sscanf(input, "%d:%d:%d:%d:%d:%d", num1, num2, num3, num4, num5, num6) == 4){}else{ /**num1 = 1;*/ }}
+
+
+// ---------------------GUARD------------------------
+#endif
+// --------------------------------------------------
