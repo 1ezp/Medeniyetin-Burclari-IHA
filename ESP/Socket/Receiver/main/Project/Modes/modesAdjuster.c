ae50032b7e4e1c3f4f1d9cee41b68b66f41540a8
DIAMOND <XXMOHAMMADXxXx754@gmail.com>
2024-07-07 21:46:43 +0300

    Added modeAdjuster

---
+++ ESP/Socket/Receiver/main/Project/Modes/modesAdjuster.c
@@ -0,0 +1,72 @@
+// ---------------------GUARD------------------------
+#ifndef ModesAdjuster
+#define ModesAdjuster
+// --------------------------------------------------
+
+bool isPID = false;
+bool targetLostOverride = false;
+
+extern bool isManualTaskOpen;
+extern bool isPIDTaskOpen;
+
+extern bool shouldCalculate;
+
+void adjustMode(const int* data){
+
+    if(data[0] == 0){
+
+        isPID = false;
+        targetLostOverride = false;
+
+        digitalWrite(pixhawlkPin, 0);
+        digitalWrite(shutdownPin, 0);
+
+        if(isManualTaskOpen){
+
+            xTaskCreate(manualTask, "manualTask", 1024*3, NULL, 1, NULL);
+        }
+    }
+    else if(data[0] == 1){
+
+        isPID = false;
+        targetLostOverride = false;
+
+        digitalWrite(pixhawlkPin, 1);
+        digitalWrite(shutdownPin, 0);
+    }
+    else if((data[0] == 2 && !targetLostOverride) || data[0] == 4){
+
+        isPID = true;
+        targetLostOverride = false;
+
+        shouldCalculate = true;
+
+        digitalWrite(pixhawlkPin, 0);
+        digitalWrite(shutdownPin, 0);
+
+        if(isPIDTaskOpen){
+
+            xTaskCreate(PIDTask, "PIDTask", 1024*3, NULL, 1, NULL);
+        }
+    }
+    else if(data[0] == 3){
+
+        isPID = false;
+        targetLostOverride = false;
+
+        digitalWrite(pixhawlkPin, 0);
+        digitalWrite(shutdownPin, 1);
+    }
+
+    if(targetLostOverride){
+
+        digitalWrite(pixhawlkPin, 1);
+        digitalWrite(shutdownPin, 0);
+    }
+}
+
+
+
+// ---------------------GUARD------------------------
+#endif
+// --------------------------------------------------
