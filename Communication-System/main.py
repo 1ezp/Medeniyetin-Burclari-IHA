98ba9d5d01b68d37170e68e2ee250add0dfe93f9
HikmeT <hekmt111@gmail.com>
2024-07-15 13:59:26 +0300

    main.py running pixHawk with Thread

--- Communication-System/main.py
+++ Communication-System/main.py
@@ -12,6 +12,6 @@ from trackSystem import main as trackSystem
 Thread(target=usbReciver.main).start()
 Thread(target=IHAConnection.start).start()
 Thread(target=socketServer.start).start()
-#Thread(target=pixHawk.start).start()
+Thread(target=pixHawk.start).start()
 
-#Thread(target=trackSystem.main).start()
+# Thread(target=trackSystem.main).start()
