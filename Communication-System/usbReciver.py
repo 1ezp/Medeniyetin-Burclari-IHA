4565e13c41281e615d59bef7fbb957a27aea0b3b
HikmeT <hekmt111@gmail.com>
2024-07-16 16:04:32 +0300

    getting the Movment, Turret for IKA

--- Communication-System/usbReciver.py
+++ Communication-System/usbReciver.py
@@ -4,25 +4,6 @@ import serial.tools.list_ports
 import os,time
 
 
-def start():
-   # ihaLat:ihaLong:x:y:mode
-   port = "COM8"
-   ser = serial.Serial(port, 115200, timeout=.1)
-
-   while True:
-      try:
-         recivedData = ser.readline().decode()
-         if recivedData != '':
-            data = recivedData.split(":")
-            #print(f"Mode:{data[4]}\r")
-            if data[0] == 0 and data[1] == 0:
-               IHA.update({"Lat": -1, "Long": -1})   
-            else: 
-               IHA.update({"Lat": float(data[0]), "Long": float(data[1])})
-               Target.update({"x":data[2],"y":data[3]})
-               print("x:",data[2],"y:",data[3])
-      except:
-         pass
 
 def connect_serial(port):
     while True:
@@ -34,27 +15,32 @@ def connect_serial(port):
   
     
 def main():
-   # MODE:controllerX:controllerY:motorSpeed
+   # MODE:controllerX:controllerY:motorSpeed:Movment:Turret
    
    port = "COM11"
    ser = connect_serial(port)
    while True:
       try:
          recivedData = ser.readline().decode()
-         print(recivedData)
+         #print(recivedData)
          if recivedData != '':
             data = recivedData.split(":")
             MODE.update({"value": data[0]})
             Controller.update({"x": data[1], "y": data[2]})
             IHA.update({"motorSpeed": int(data[3])})
+            IKA.update({"Movment":int(data[4]),"Turret":int(data[5])})
+
+            
       except serial.SerialException:
          MODE.update({"value": 1})
          if ser and ser.isOpen():
             ser.close()
          ser = connect_serial(port)
+
       except Exception as e:
          MODE.update({"value": 1})
          print(e)
 
 if __name__ == "__main__":
    main()
+   
