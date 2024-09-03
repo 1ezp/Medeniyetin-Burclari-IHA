1dabaebffbfd735eadd91c6f675e2b6f4671051f
HikmeT <hekmt111@gmail.com>
2024-09-03 16:26:44 +0300

    Update main.py

--- Communication-System/main.py
+++ Communication-System/main.py
@@ -1,19 +1,24 @@
-from data import *
-import serial
 from threading import Thread
+from data import *
+
+from pixHawk import Pixhawk
 import socketServer
 import usbReciver
+
 import IHAConnection
 import IKAConnection
-import pixHawk
 
+import PID
 from trackSystem import main as trackSystem
 
+pixy = Pixhawk('udp:127.0.0.1:14550')
 
-Thread(target=usbReciver.main).start()
-Thread(target=IHAConnection.start).start()
-Thread(target=IKAConnection.start).start()
-Thread(target=socketServer.start).start()
-Thread(target=pixHawk.start).start()
+Thread(target=pixy.fetchData).start()
+Thread(target=socketServer.start, args=(pixy,)).start()
+# Thread(target=usbReciver.main).start()
 
-Thread(target=trackSystem.main).start()
+# Thread(target=IHAConnection.start).start()
+# Thread(target=IKAConnection.start).start()
+
+# Thread(target=trackSystem.main).start()
+Thread(target=PID.start, args=(pixy,)).start()
