e8aa22052d0fb4dc94b43d4586de3737670c05c5
HikmeT <hekmt111@gmail.com>
2024-09-08 13:00:08 +0300

    Update main.py

--- Communication-System/main.py
+++ Communication-System/main.py
@@ -9,16 +9,18 @@ import IHAConnection
 import IKAConnection
 
 import PID
-from trackSystem import main as trackSystem
+from trackSystem.main import VideoTracker
 
-pixy = Pixhawk('udp:127.0.0.1:14550')
+# pixy = Pixhawk('udp:127.0.0.1:14550')
+pixy = Pixhawk('tcp:127.0.0.1:5760')
 
-Thread(target=pixy.fetchData).start()
-Thread(target=socketServer.start, args=(pixy,)).start()
+tracker = VideoTracker("http://192.168.10.123:7123/stream.mjpg")
+# Thread(target=pixy.fetchData).start()
+# Thread(target=socketServer.start, args=(pixy,)).start()
 # Thread(target=usbReciver.main).start()
 
 # Thread(target=IHAConnection.start).start()
 # Thread(target=IKAConnection.start).start()
 
-# Thread(target=trackSystem.main).start()
-Thread(target=PID.start, args=(pixy,)).start()
+Thread(target=tracker.start).start()
+# Thread(target=PID.start, args=(pixy,)).start()
